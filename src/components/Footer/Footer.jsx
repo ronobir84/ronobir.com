18bd208fc18eebd1b6f6068b35a07b78f503e236
ronodir84 <priashdas@84gmail.com>
2024-06-07 00:36:24 +0600

    add the last section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className=" text-sm p-4 text-center footer__section">
-      Copyright © 2023 Ronobir Das. All Rights reserved.
+      Copyright © 2024 Ronobir Das. All Rights reserved.
     </div>
   );
 };
--- src/oldComponents/Footer/Footer.jsx
+++ src/oldComponents/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-gray-800 text-sm p-4 text-center text-white">
-      Copyright © 2022 Ronobir DAS.All Rights reserved.
+      Copyright © 2024 Ronobir DAS.All Rights reserved.
     </div>
   );
 };
